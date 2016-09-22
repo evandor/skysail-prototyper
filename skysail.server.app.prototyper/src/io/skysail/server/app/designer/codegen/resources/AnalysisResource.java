48f0b4cb7463011ffd94c4caffc49f47fa6e84fe
Carsten Gräf <evandor@gmail.com>
2016-09-22 09:10:58 +0200

    wip

---
+++ skysail.server.app.prototyper/src/io/skysail/server/app/designer/codegen/resources/AnalysisResource.java
@@ -0,0 +1,21 @@
+package io.skysail.server.app.designer.codegen.resources;
+
+import io.skysail.api.responses.SkysailResponse;
+import io.skysail.server.app.designer.application.DbApplication;
+import io.skysail.server.restlet.resources.EntityServerResource;
+
+public class AnalysisResource extends EntityServerResource<DbApplication> {
+
+    @Override
+    public SkysailResponse eraseEntity() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object getEntity() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
